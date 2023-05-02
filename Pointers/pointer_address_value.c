699244c8f52c40c8519fcaf7bec70a494237a92f
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-05-02 13:45:00 +0530

    Create pointer_address_value.c

---
+++ Pointers/pointer_address_value.c
@@ -0,0 +1,15 @@
+// Progarm to demonstrate the address & the value of pointer variable
+#include <stdio.h>
+main()
+{
+  int x,y,*ptr;
+  ptr = &x;
+  x = 10;
+  y = *ptr;
+  printf("Value of x is %d\n",x);
+  printf("%d is stored at address = %u\n",*&x,&x);
+  printf("%d is stored at address = %u\n",*ptr,ptr);
+  printf("%u is stored at address = %u\n",ptr,&ptr);
+  *ptr = 25;
+  printf("x = %d\n",x);
+}
