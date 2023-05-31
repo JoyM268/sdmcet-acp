3e8b6060de1773d12bacd462d0edf662a40be493
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-05-31 20:57:32 +0530

    Add files via upload

---
+++ Pointers/binary_search.c
@@ -0,0 +1,58 @@
+//Modular program to implement binary search
+#include<stdio.h>
+#define SIZE 50
+
+void read_array(int *, int);
+int binary_search(int *, int, int);
+main()
+{
+    int arr[SIZE], n, key, index;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements: ", n);
+    read_array(arr, n);
+    printf("Enter the key element: ");
+    scanf("%d", &key);
+    index = binary_search(arr, n, key);
+    if(index != -1)
+    {
+        printf("%d found at index %d\n", key, index);
+    }
+    else
+    {
+        printf("%d not found\n", key);
+    }
+}
+
+int binary_search(int *arr, int n, int key)
+{
+    int low = 0, high = n - 1, mid;
+    while(low <= high)
+    {
+        mid = (low + high)/2;
+        if(key == arr[mid])
+        {
+            return mid;
+        }
+        else if(key < arr[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+void read_array(int *arr, int n)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+}
+
+
