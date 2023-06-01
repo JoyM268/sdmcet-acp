64c8fe34185487038767b3540ae3830a52f69334
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-06-01 14:13:46 +0530

    Add files via upload

---
+++ Assignment - 1/q11.c
@@ -0,0 +1,56 @@
+//Program to add principal diagonal elements of the array(primary and secondary diagonal)
+#include<stdio.h>
+#include<stdlib.h>
+#define ROW 15
+#define COL 15
+
+void read_matrix(int (*)[COL], int);
+int primary(int (*)[COL], int);
+int secondary(int (*)[COL], int);
+
+main()
+{
+  int matrix[ROW][COL], m, n;
+  printf("Enter the order of matrix: ");
+  scanf("%d %d", &m, &n);
+  if(m != n)
+  {
+    printf("Error: It is not a square matrix\n");
+    exit(0);
+  }
+  printf("Enter %d elements:\n", m*n);
+  read_matrix(matrix, m);
+  printf("The sum of primary diagonal elements: %d\n", primary(matrix, m));
+  printf("The sum of secondary diagonal elements: %d\n",secondary(matrix, m));
+}
+
+void read_matrix(int (*matrix)[COL], int m)
+{
+  int i, j;
+  for(i = 0; i < m; i++)
+  {
+    for(j = 0; j < m; j++)
+    {
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+}
+int primary(int (*matrix)[COL], int m)
+{
+  int sum = 0, i;
+  for(i = 0; i < m; i++)
+  {
+    sum += matrix[i][i];
+  }
+  return sum;
+}
+
+int secondary(int (*matrix)[COL], int m)
+{
+  int sum = 0, i;
+  for(i = 0; i < m; i++)
+  {
+    sum += matrix[i][m - i - 1];
+  }
+  return sum;
+}
