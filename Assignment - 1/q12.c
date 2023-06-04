6e10ca2e12b055efea1dadcb3885c53e3207f96b
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-06-04 17:34:35 +0530

    Update q12.c

--- Assignment - 1/q12.c
+++ Assignment - 1/q12.c
@@ -4,8 +4,7 @@
 #define COL 15
 
 void read_matrix(int (*)[COL], int, int);
-void swap(int *, int *);
-void transpose_matrix(int (*)[COL], int (*)[COL], int *, int *);
+void transpose_matrix(int (*)[COL], int (*)[COL]);
 void print_matrix(int (*)[COL], int, int);
 main()
 {
@@ -16,28 +15,19 @@ main()
     read_matrix(matrix, m, n);
     transpose_matrix(matrix, transpose, &m, &n);
     printf("The transpose of the matrix is:\n");
-    print_matrix(transpose, m, n);
+    print_matrix(transpose, n, m);
 }
 
-void swap(int *m, int *n)
-{
-    int temp;
-    temp = *m;
-    *m = *n;
-    *n = temp;
-}
-
-void transpose_matrix(int (*matrix)[COL], int (*transpose)[COL], int *m, int *n)
+void transpose_matrix(int (*matrix)[COL], int (*transpose)[COL], int m, int n)
 {
     int i, j;
-    for(i = 0; i < *m; i++)
+    for(i = 0; i < m; i++)
     {
-        for(j = 0; j < *n; j++)
+        for(j = 0; j < n; j++)
         {
             transpose[j][i] = matrix[i][j];
         }
     }
-    swap(m, n);
 }
 
 void read_matrix(int (*matrix)[COL], int m, int n)
