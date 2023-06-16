9dab6e83d48940b51f7b62e676e1c2ca445be240
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-06-16 11:13:11 +0530

    Update term_work_2.c

--- Term Work/term_work_2.c
+++ Term Work/term_work_2.c
@@ -1,55 +1,54 @@
-/* Using pointer write a function that receives a character string and a character as argument and delete all occurrences of this character in the string. The function should return the corrected string with no holes. */
-
-#include <stdio.h>
-#define SIZE 50 
-
-void delete_character(char *, char);
-main() 
-{
-  char str[SIZE], ch;
-  printf("Enter a input string: ");
-  scanf("%s", str);
-  getchar();
-  printf("Enter a character to be deleted: ");
-  scanf("%c", &ch);
-  delete_character(str, ch);
-  printf("The string after deletion of %c from string is:\n%s\n", ch, str);
-}
-
-void delete_character(char *str, char ch) 
-{
-  char *temp;
-  while(*str)
-  {
-    if(*str == ch)
-    {
-      temp = str;
-      while(*temp)
-      {
-        *temp = *(temp + 1);
-        temp++;
-      }
-      continue;
-    }
-    str++;
-  }
-}
-
-//OR (alternate Method)
-
-/*
- void delete_character(char *str, char ch)
-{
-  char *dest = str;
-  while (*str)
-  {
-    if (*str != ch)
-    {
-      *dest = *str; 
-      dest++;      
-    }
-    str++; 
-  }
-  *dest = '\0';
-}
-*/
+/* Using pointer write a function that receives a character string and a character as argument and delete all occurrences of this character in the string. The function should return the corrected string with no holes. */ 
+  
+ #include <stdio.h> 
+ #define SIZE 50  
+  
+ void delete_character(char *, char); 
+ main()  
+ { 
+   char str[SIZE], ch; 
+   printf("Enter a input string: "); 
+   scanf("%s", str); 
+   getchar(); 
+   printf("Enter a character to be deleted: "); 
+   scanf("%c", &ch); 
+   delete_character(str, ch); 
+   printf("The string after deletion of %c from string is:\n%s\n", ch, str); 
+ } 
+  
+ void delete_character(char *str, char ch) 
+ { 
+   char *dest = str; 
+   while (*str) 
+   { 
+     if (*str != ch) 
+     { 
+       *dest = *str;  
+       dest++;       
+     } 
+     str++;  
+   } 
+   *dest = '\0'; 
+ } 
+ 
+ //OR (alternate Method) 
+ /*
+ void delete_character(char *str, char ch)  
+ { 
+   char *temp; 
+   while(*str) 
+   { 
+     if(*str == ch) 
+     { 
+       temp = str; 
+       while(*temp) 
+       { 
+         *temp = *(temp + 1); 
+         temp++; 
+       } 
+       continue; 
+     } 
+     str++; 
+   } 
+ } 
+ */
