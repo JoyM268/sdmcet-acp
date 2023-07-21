3d07144418a718d672257d890164150b8f179182
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-07-21 20:19:18 +0530

    Update and rename testfile.md to frequency.c

---
+++ Storage Classes and Dynamic Memory Allocation/frequency.c
@@ -0,0 +1,34 @@
+// Program to input a list of numbers within the range 0 - 4 from the user and then find the frequency of each of them
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+    int *num, *freq, i, size;
+    printf("Enter the size of the list: ");
+    scanf("%d", &size);
+    num = (int *)malloc(size * sizeof(int));
+    printf("Enter the numbers:\n");
+    for(i = 0; i < size; i++)
+    {
+        printf("Enter num[%d]: ", i);
+        scanf("%d", &num[i]);
+        if(num[i] < 0 || num[i] > 4)
+        {
+            printf("The number should br within the range of (0 - 4)\n");
+            i--;
+            continue;
+        }
+    }
+    freq = (int *)calloc(5, sizeof(int));
+    for(i = 0; i < size; i++)
+    {
+        freq[num[i]]++;
+    }
+    printf("The frequencies of the number are:\n");
+    for(i = 0; i < 5; i++)
+    {
+        printf("Frequency[%d] = %d\n", i, freq[i]);
+    }
+    free(num);
+    free(freq);
+}
