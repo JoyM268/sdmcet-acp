bc5b10ada9dd5b4d93f6b53c21e9e6c649322856
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-07-25 20:01:13 +0530

    Add files via upload

---
+++ Extras/input_arguments.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(int argc, char *argv[])
+{
+    FILE *fp;
+    char words[50];
+    int i;
+    if(argc < 3)
+    {
+        printf("Insufficient commands\n");
+        exit(1);
+    }
+    fp = fopen(argv[1], "w");
+    for(i = 2; i < argc; i++)
+    {
+        fprintf(fp, "%s ", argv[i]);
+    }
+    fclose(fp);
+    fp = fopen(argv[1], "r");
+    printf("The words stored in file are: ");
+    for(i = 2; i < argc; i++)
+    {
+        fscanf(fp, "%s", &words);
+        fprintf(stdout, "%s ", words);
+    }
+    fclose(fp);
+}
