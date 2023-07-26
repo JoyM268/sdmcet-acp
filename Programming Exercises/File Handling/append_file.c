426b68a12a8109d9e44b5c409611c809c4f75ed7
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-07-26 20:49:50 +0530

    Add files via upload

---
+++ Programming Exercises/File Handling/append_file.c
@@ -0,0 +1,25 @@
+//Write a program that appends one file at the end of another
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+    FILE *fp1, *fp2;
+    char ch, file1[100], file2[100];
+    printf("Enter the name of file 1: ");
+    scanf("%s", file1);
+    printf("Enter the name of file 2: ");
+    scanf("%s", file2);
+    fp1 = fopen(file1, "a");
+    fp2 = fopen(file2, "r");
+    if(fp1 == NULL || fp2 == NULL)
+    {
+        printf("Error opening the file\n");
+        exit(1);
+    }
+    while((ch = getc(fp2)) != EOF)
+    {
+        putc(ch, fp1);
+    }
+    printf("file %s successfully appended into file %s\n", file2, file1);
+}
