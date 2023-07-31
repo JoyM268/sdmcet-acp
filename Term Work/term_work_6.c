a5fcc55666f89f877a84b60eb9b5641cfa91b513
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-07-31 19:47:32 +0530

    Add files via upload

---
+++ Term Work/term_work_6.c
@@ -0,0 +1,57 @@
+/*
+Write a C program to open a file named INVENTORY and store in it the following data.: 
+Item name   Number    Price     Quantity  
+AAA-1        111        17.5       115 
+BBB-2        125         36         75 
+CCC-3        247        31.75      104
+Extend the program  
+i. To  read  the  data  from  the  file  INVENTORY  and display the inventory table with the values of each item. 
+ii.  Append  additional  items  to  the  file  INVENTORY and print the total contents of the file.
+*/
+#include<stdio.h>
+
+main()
+{
+    FILE *fp;
+    char itemName[100];
+    int number, quantity, i;
+    float price;
+    long n;
+    fp = fopen("INVENTORY", "w");
+    fprintf(fp, "Item name\tNumber\tPrice\tQuantity\n");
+    n = ftell(fp);
+    for(i = 1; i <= 3; i++)
+    {
+        printf("Enter the name, number, price and quantity of item %d: ", i);
+        scanf("%s %d %f %d", itemName, &number, &price, &quantity);
+        fprintf(fp, "%s\t\t%d\t%.2f\t%d\n", itemName, number, price, quantity);
+    }
+    fclose(fp);
+    fp = fopen("INVENTORY", "r");
+    fseek(fp, n, 0);
+    printf("\nInventory table:\n");
+    printf("Item name\tNumber\tPrice\tQuantity\n");
+    while(1)
+    {
+        fscanf(fp, "%s %d %f %d", itemName, &number, &price, &quantity);
+        if(feof(fp)) break;
+        printf("%s\t\t%d\t%.2f\t%d\n", itemName, number, price, quantity);
+    }
+    fclose(fp);
+    fp = fopen("INVENTORY", "a");
+    printf("\nEnter the name, number, price and quantity of the item to be appended: ");
+    scanf("%s %d %f %d", itemName, &number, &price, &quantity);
+    fprintf(fp, "%s\t\t%d\t%.2f\t%d\n", itemName, number, price, quantity);
+    fclose(fp);
+    fp = fopen("INVENTORY", "r");
+    fseek(fp, n, 0);
+    printf("\nInventory table:\n");
+    printf("Item name\tNumber\tPrice\tQuantity\n");
+    while(1)
+    {
+        fscanf(fp, "%s %d %f %d", itemName, &number, &price, &quantity);
+        if(feof(fp)) break;
+        printf("%s\t\t%d\t%.2f\t%d\n", itemName, number, price, quantity);
+    }         
+    fclose(fp);
+}
