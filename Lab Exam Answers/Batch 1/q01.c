231547cb922e95f0ebd7c5510c9bdbccbf705589
Joy Mascarenhas <125041935+JoyM268@users.noreply.github.com>
2023-08-25 18:37:48 +0530

    Update q01.c

--- Lab Exam Answers/Batch 1/q01.c
+++ Lab Exam Answers/Batch 1/q01.c
@@ -34,3 +34,20 @@ int is_palindrome(char *str, int len)
     }
     return 1;
 }
+
+/* OR
+
+int is_palindrome(char *str, int len)
+{
+    int i;
+    for(i = 0; i < len; i++)
+    {
+        if(*(str + i) != *(str + (len - i - 1)))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+*/
